2cec059491f06c336c72e732cb9ff7cc9900425d
Raindevops <raindevops.durand@gmail.com>
2021-03-12 18:05:33 -0800

    add font

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,8 @@
 
 <Router url={url}>
     <header>
+        <span><Link to="/">LotR - Svelte</Link></span>
         <nav>
-            <Link to="/">LotR - Svelte</Link>
             <Link to="/Books">Books</Link>
             <Link to="/Movies">Movies</Link>
             <Link to="/Quotes">Quotes</Link>
@@ -40,10 +40,37 @@
     $orange : #ff3e00;
     header {
         width: 100%;
-        display: flex;
         justify-content: space-between;
         background-color: $orange;
-        color: white;
+        display: flex;
+        nav, span {
+            margin: 1rem 0;
+            padding: 0 .75rem;
+        }
+        :global(a) {
+            color:white;
+            text-decoration: none;
+            display: inline-block;
+            &:after{
+                content:'';
+                width: 0;
+                background-color: white;
+            }
+            &:not(:first-child){
+                margin-left: 1rem;
+            }
+            &[aria-current="page"]{
+                position: relative;
+                &:after{
+                    display: block;
+                    transition: width .5s ease-in-out;
+                    width: 100%;
+                    height: 2px;
+                    border-radius: .5rem;
+                    margin-top: .5rem;
+                }
+            }
+        }
     }
     main {
         text-align: center;
