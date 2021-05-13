fbf57d8d24534df70c96a7ab3df49c332f6c01cb
Raindevops <raindevops.durand@gmail.com>
2021-05-13 22:44:12 +0200

    get unic list of realm & race

--- src/Component/Characters/Characters.svelte
+++ src/Component/Characters/Characters.svelte
@@ -1,13 +1,16 @@
 <script>
     import {onMount} from "svelte";
-
+    import SingleCharacter from "./SingleCharacter.svelte"
     let characters = []
+
+    let race;
+    let realm;
 
     onMount(async function () {
 
         if (localStorage.getItem("characters")) {
             characters = JSON.parse(localStorage.getItem("characters"));
-            console.log(characters[0])
+            distinctArray(characters);
             return;
         }
 
@@ -22,18 +25,61 @@
             characters = data.docs;
             localStorage.setItem("characters", JSON.stringify(data.docs));
         });
+
     })
+
+    const distinctArray = datas => {
+        const raceArr = [];
+        const realmArr = [];
+        datas.map(d => {
+            if (d.race.indexOf(",") > -1) {
+                let dts = d.race.split(",");
+                dts.map(el => {
+                    !raceArr.includes(el) ? raceArr.push(el) : false;
+                });                
+            }else if (!raceArr.includes(d.race) && (d.race !== "NaN" && d.race.length > 0)) {
+                raceArr.push(d.race);
+            }
+            
+            if (d.realm.indexOf(",") > -1){
+                let dts = d.realm.split(",");
+                dts.map(el=>{
+                    !realmArr.includes(el) ? realmArr.push(el) : false;
+                });
+            } else if (!realmArr.includes(d.realm) && (d.realm !== "NaN" && d.realm.length > 0)) {
+                realmArr.push(d.realm);
+            }
+        });
+        console.log(raceArr)
+        console.log(realmArr)
+    }
+    
 </script>
 
 <main>
     <h1>Characters section</h1>
+    
     {#if characters}
         {#await characters}
             <p>Is loading</p>
             {:then characters} 
-            {#each characters as character }
-                <p>{character.name}</p>
-            {/each}
+            <div class="character-research">
+                <select value={race} name="race">
+                    {#each characters as character }
+                        <option value={character.race}>
+                            {character.race}
+                        </option>
+                    {/each}
+                </select>
+                <select value={realm} name="realm">
+                    {#each characters as character }
+                        <option value={character.realm}>
+                            {character.realm}
+                        </option>
+                    {/each}
+                </select>
+            </div>
+            <!-- <SingleCharacter character={character}/> -->
         {/await}
     {/if}
 </main>
---
+++ src/Component/Characters/SingleCharacter.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let character;
+</script>
+
+<div class="character">
+    <h3><a href={character.wikiUrl}>{character.name}</a></h3>
+    <ul>
+        {#each Object.entries(character) as [key, val] }
+            {#if val && key !== "_id" && key !== "wikiUrl" && key !=="name"}
+                <li>
+                    <span class="key">{key} :</span>  
+                    <span class="val">{val}</span>
+                </li>
+            {/if}
+        {/each}
+    </ul>
+</div>
+
+<style type="text/scss">
+$gold : #C4B479;
+.character{
+    margin-bottom: 2rem;
+    h3{
+        margin: 0;
+        a{
+            color: $gold;
+        }
+    }
+    ul{
+        list-style: none;
+        margin-left: 0;
+        li{
+            .key{
+                text-transform: capitalize;
+            }
+        }
+    }
+}
+</style>
